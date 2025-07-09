50bb348e0d64c5e57843d1ad0ebdf73b6dfeeefd
Gouravpatidar9165 <gouravpatidar9165@gmail.com>
2025-07-09 21:26:33 +0530

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -101,7 +101,7 @@ const ContactSection: React.FC = () => {
                 
                 <div className="flex items-start">
                   <div className="mr-3 p-3 rounded-full bg-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="white" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="black" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                       <polyline points="22,6 12,13 2,6"></polyline>
                     </svg>
@@ -114,7 +114,7 @@ const ContactSection: React.FC = () => {
                 
                 <div className="flex items-start">
                   <div className="mr-3 p-3 rounded-full bg-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="white" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="black" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                       <circle cx="12" cy="10" r="3"></circle>
                     </svg>
